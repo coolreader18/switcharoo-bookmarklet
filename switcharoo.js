ee6358fbb16c978d2e580089cb02bf1aa99eab8e
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-12-11 09:00:44 -0600

    Error if there's no permalinked comment

--- switcharoo.js
+++ switcharoo.js
@@ -1,27 +1,31 @@
-$.getJSON("https://api.reddit.com/r/switcharoo/new?limit=1").done(function(data) {
-  $(".border ~ .buttons .reply-button a").click();
-  var editarea = $($(".border").parents()[1].childNodes[3].childNodes[0].childNodes[1]);
-  editarea.find("textarea")[0].value = "Ah, the ol' reddit [switcharoo](" + data.data.children[0].data.url + ")! \n\n(make this creative/relevant and remove this message, then click save)";
-  editarea.find("button.save").click(function() {
-    wait()
-    function wait() {
-      var dataperm = $(".border").parents()[1].childNodes[3].childNodes[0].childNodes[0].attributes.getNamedItem("data-permalink");
-      if (!dataperm) {
-        setTimeout(wait, 100);
-      } else {
-        var newroo = "https://www.reddit.com" + dataperm.value;
-        var intent = prompt("What was the intended subject?", "");
-        var switched = prompt("What was it switched to?", "");
-        var context = prompt("How many comments before your switcharoo comment would someone need to read to understand it?", "");
-        var submiturl = "https://www.reddit.com/r/switcharoo/submit?url=" + encodeURI(newroo + "?context=" + context) + "&title=" + encodeURI(intent + " vs " + switched);
-        if (prompt("Please read the rules on the sidebar of the popup to make sure that your post follows them. To confirm that you can handle that reponsibility, type in \"switcharoo\".") == "switcharoo") {
-          window.open(submiturl, "_blank");
+if ($(".border").length) {
+  $.getJSON("https://api.reddit.com/r/switcharoo/new?limit=1").done(function(data) {
+    $(".border ~ .buttons .reply-button a").click();
+    var editarea = $($(".border").parents()[1].childNodes[3].childNodes[0].childNodes[1]);
+    editarea.find("textarea")[0].value = "Ah, the ol' reddit [switcharoo](" + data.data.children[0].data.url + ")! \n\n(make this creative/relevant and remove this message, then click save)";
+    editarea.find("button.save").click(function() {
+      wait()
+      function wait() {
+        var dataperm = $(".border").parents()[1].childNodes[3].childNodes[0].childNodes[0].attributes.getNamedItem("data-permalink");
+        if (!dataperm) {
+          setTimeout(wait, 100);
         } else {
-          alert("Just be responsible next time");
+          var newroo = "https://www.reddit.com" + dataperm.value;
+          var intent = prompt("What was the intended subject?", "");
+          var switched = prompt("What was it switched to?", "");
+          var context = prompt("How many comments before your switcharoo comment would someone need to read to understand it?", "");
+          var submiturl = "https://www.reddit.com/r/switcharoo/submit?url=" + encodeURI(newroo + "?context=" + context) + "&title=" + encodeURI(intent + " vs " + switched);
+          if (prompt("Please read the rules on the sidebar of the popup to make sure that your post follows them. To confirm that you can handle that reponsibility, type in \"switcharoo\".") == "switcharoo") {
+            window.open(submiturl, "_blank");
+          } else {
+            alert("Just be responsible next time");
+          }
         }
       }
-    }
+    });
+  }).fail(function() {
+    alert("Couldn't reach reddit, it may be blocked or down")
   });
-}).fail(function() {
-  alert("Couldn't reach reddit, it may be blocked or down")
-});
+} else {
+  alert("Couldn't find the permalinked comment, please click the \"permalink\" button underneath the comment that switches the subjects");
+}
