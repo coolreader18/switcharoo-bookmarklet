1a5ef355a3615c882547e4aae85d379d2ed10bc3
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-03 17:19:43 +0200

    optimised with jquery and stuff

--- switcharoo.js
+++ switcharoo.js
@@ -4,33 +4,38 @@
 // ==/Bookmarklet==
 
 if ($(".border").length) {
-  $.getJSON("https://api.reddit.com/r/switcharoo/new?limit=1").done(function(data) {
+  $.getJSON("https://api.reddit.com/r/switcharoo/new?limit=1").done(data => {
     $(".border ~ .buttons .reply-button a").click();
-    var editarea = $($(".border").parents()[1].childNodes[3].childNodes[0].childNodes[1]);
-    editarea.find("textarea")[0].value = "Ah, the ol' reddit [switcharoo](" + data.data.children[0].data.url + ")! \n\n(make this creative/relevant and remove this message, then click save)";
-    editarea.find("button.save").click(function() {
-      wait()
-      function wait() {
-        var dataperm = $(".border").parents()[1].childNodes[3].childNodes[0].childNodes[0].attributes.getNamedItem("data-permalink");
-        if (!dataperm) {
-          setTimeout(wait, 100);
+    var editarea = $(".border").parents().eq(1).find(".usertext-edit");
+    editarea.find("textarea").val(`Ah, the ol' reddit [switcharoo](${data.data.children[0].data.url})!
+
+(make this creative/relevant and remove this message, then click save`);
+    editarea.find("button.save").click(() => {
+      var intrvl = setInterval(() => {
+        var perm = $(".border").parents().find(".child").children().eq(0).children().eq(0).data("permalink");
+        if (!perm) {
+          clearInterval(intrvl);
         } else {
-          var newroo = "https://www.reddit.com" + dataperm.value;
-          var intent = prompt("What was the intended subject?", "");
-          var switched = prompt("What was it switched to?", "");
-          var context = prompt("How many comments before your switcharoo comment would someone need to read to understand it?", "");
-          var submiturl = "https://www.reddit.com/r/switcharoo/submit?url=" + encodeURI(newroo + "?context=" + context) + "&title=" + encodeURI(intent + " vs " + switched);
+          var newroo = `https://www.reddit.com${perm}`,
+          intent = prompt("What was the intended subject?"),
+          switched = prompt("What was it switched to?"),
+          context = prompt("How many comments before your switcharoo comment would someone need to read to understand it?"),
+          submiturl = new URL("https://www.reddit.com/r/switcharoo/submit"),
+          search = submiturl.searchParams;
+          search.append("url", newroo);
+          search.append("context", context);
+          search.append("title", `${intent} vs ${switched}`);
           if (prompt("Please read the rules on the sidebar of the popup to make sure that your post follows them. To confirm that you can handle that reponsibility, type in \"switcharoo\".") == "switcharoo") {
-            window.open(submiturl, "_blank");
+            window.open(submiturl.toString(), "_blank");
           } else {
             alert("Just be responsible next time");
           }
         }
-      }
+      }, 10);
     });
-  }).fail(function() {
-    alert("Couldn't reach reddit, it may be blocked or down")
+  }).fail(() => {
+    alert(new Error("Couldn't reach reddit, it may be blocked or down"))
   });
 } else {
-  alert("Couldn't find the permalinked comment, please click the \"permalink\" button underneath the comment that switches the subjects");
+  alert(new Error("Couldn't find the permalinked comment, please click the \"permalink\" button underneath the comment that switches the subjects"));
 }
